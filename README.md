adf79d53b5342ee359a2584eb5bd31b2176d4c55
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2018-05-17 12:15:58 -0400

    README and ros install script

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install Scripts
+
+This repo contains a collection of install scripts I have created over the years
+to install software that is not easily installed via `apt install`.
+The scripts are meant to be used on the latest
+[LTS](https://wiki.ubuntu.com/LTS) release of Ubuntu.
---
+++ ros_install.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+UBUNTU_CODENAME=$(lsb_release -s -c)
+case $UBUNTU_CODENAME in
+    trusty)
+        ROS_DISTRO=indigo;;
+    zesty)
+        ROS_DISTRO=lunar;;
+    xenial)
+        ROS_DISTRO=kinetic;;
+    *)
+        echo "Unable to match Ubuntu release (named $UBUNTU_CODENAME) to a ROS version"
+    exit 1
+esac
+
+sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $UBUNTU_CODENAME main\" > /etc/apt/sources.list.d/ros-latest.list"
+wget -qO - http://packages.ros.org/ros.key | sudo apt-key add -
+
+echo "Updating package lists ..."
+sudo apt-get -qq update
+
+echo "Installing ROS $ROS_DISTRO ..."
+sudo apt-get -qq install ros-$ROS_DISTRO-desktop python-rosinstall > /dev/null
+
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+# Prepare rosdep to install dependencies.
+echo "Updating rosdep ..."
+if [ ! -d /etc/ros/rosdep ]; then
+    sudo rosdep init > /dev/null
+fi
+rosdep update > /dev/null
