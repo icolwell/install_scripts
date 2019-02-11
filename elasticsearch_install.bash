92f4f4ddb82de8849f2d40baa62f6a952f911d6d
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2019-02-11 10:19:37 -0500

    elasticsearch install

--- elasticsearch_install.bash
+++ elasticsearch_install.bash
@@ -2,7 +2,7 @@
 set -e
 
 # Source:
-# https://www.elastic.co/guide/en/logstash/current/installing-logstash.html#package-repositories
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
@@ -10,6 +10,6 @@ echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee 
 echo "Updating package lists ..."
 sudo apt update -qq
 
-sudo apt -y install logstash
+sudo apt -y install elasticsearch
 
 echo "Done :D"
