08917d3ba6d5b60b3b2e857a08a94e19c7a02a31
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2019-11-11 15:14:15 -0500

    Update Spotify install script

--- spotify_install.bash
+++ spotify_install.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-# Spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-echo 'deb http://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list
+# https://www.spotify.com/us/download/linux/
+
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 echo "Updating package lists ..."
-sudo apt update -qq
+sudo apt-get update -qq
 
-sudo apt -y install spotify-client
+sudo apt-get -y install spotify-client
 
-echo "Done :D"
+echo "Spotify installed successfully."
