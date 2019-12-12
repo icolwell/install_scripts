23e90b68367d8e37202187162c63590648daaad4
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2019-12-11 21:19:10 -0500

    Remove append on signal install script

--- signal_install.bash
+++ signal_install.bash
@@ -2,7 +2,7 @@
 set -e
 
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal.list
 
 echo "Updating package lists ..."
 sudo apt update -qq
