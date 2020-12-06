750ccf9d4175222249ca30e010cda14c64087de5
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2020-12-06 10:03:39 -0500

    Update key URL according to official instructions (#14)
    * Update key URL according to official instructions
    
    * Fix rpimonitor install script

--- rpimonitor_install.bash
+++ rpimonitor_install.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-# https://rpi-experiences.blogspot.com/p/rpi-monitor-installation.html
+# https://xavierberger.github.io/RPi-Monitor-docs/11_installation.html+
+# https://github.com/XavierBerger/RPi-Monitor/issues/267
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
-sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
+echo "deb https://www.giteduberger.fr rpimonitor/" | sudo tee /etc/apt/sources.list.d/rpimonitor.list
+
 sudo apt-get update
 sudo apt-get -y install rpimonitor
 sudo /etc/init.d/rpimonitor update
--- spotify_install.bash
+++ spotify_install.bash
@@ -3,8 +3,7 @@ set -e
 
 # https://www.spotify.com/us/download/linux/
 
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 echo "Updating package lists ..."
