9cd43f85f7ff50f52097ddf79408e0e07668577c
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2020-12-06 12:03:23 -0500

    Update default Godot version (#13)
    * Update godot install version, add option to specify version on command line
    
    * Small improvements

--- godot_install.bash
+++ godot_install.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="3.2.1"
+VERSION=${1:-'3.2.3'}
 FILENAME="Godot_v$VERSION-stable_x11.64"
 URL="https://downloads.tuxfamily.org/godotengine/$VERSION/$FILENAME.zip"
 INSTALL_DIR="$HOME/opt/godot-$VERSION"
@@ -13,8 +13,8 @@ wget --quiet --show-progress "$URL" -O godot.zip
 
 mkdir -p "$INSTALL_DIR"
 mkdir -p "$HOME/bin"
-unzip godot.zip -d "$INSTALL_DIR"
-ln -s "$INSTALL_DIR/$FILENAME" "$HOME/bin/godot"
+unzip -o godot.zip -d "$INSTALL_DIR"
+ln -sf "$INSTALL_DIR/$FILENAME" "$HOME/bin/godot"
 
 echo "Godot Installed to:"
 echo "$INSTALL_DIR"
