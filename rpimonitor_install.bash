680311f4ba9e8738ce2d668c4e95f0a5c370bb3a
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2022-05-18 22:17:49 -0400

    Revert rpimonitor_install.bash back to using http instead of https

--- rpimonitor_install.bash
+++ rpimonitor_install.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# https://xavierberger.github.io/RPi-Monitor-docs/11_installation.html+
+# https://xavierberger.github.io/RPi-Monitor-docs/11_installation.html
 # https://github.com/XavierBerger/RPi-Monitor/issues/267
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
-echo "deb https://www.giteduberger.fr rpimonitor/" | sudo tee /etc/apt/sources.list.d/rpimonitor.list
+echo "deb http://www.giteduberger.fr rpimonitor/" | sudo tee /etc/apt/sources.list.d/rpimonitor.list
 
 sudo apt-get update
 sudo apt-get -y install rpimonitor
